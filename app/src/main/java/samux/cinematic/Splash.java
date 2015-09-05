3f71d2f30287f30086df7c554827729c41722d5b
Yan <Yonkishum@gmail.com>
2015-09-05 10:14:19 -0430

    added a splash on a app

--- app/src/main/java/samux/cinematic/Splash.java
+++ app/src/main/java/samux/cinematic/Splash.java
@@ -1,38 +1,39 @@
 package samux.cinematic;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
 public class Splash extends ActionBarActivity {
 
+    // Splash screen timer
+    // this is a miliseconds example 3000 ms.
+    private static int SPLASH_TIME_OUT = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.hello);
-    }
+        setContentView(R.layout.splash);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.splash_resources, menu);
-        return true;
-    }
+        new Handler().postDelayed(new Runnable() {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(Splash.this, MainActivity.class);
+                startActivity(i);
 
-        return super.onOptionsItemSelected(item);
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 }

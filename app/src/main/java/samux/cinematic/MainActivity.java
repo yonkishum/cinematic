eac436b0e8c331714c966c2447c1c7ec4ff08835
Yan <Yonkishum@gmail.com>
2015-09-06 16:25:31 -0430

    login working

--- app/src/main/java/samux/cinematic/MainActivity.java
+++ app/src/main/java/samux/cinematic/MainActivity.java
@@ -1,10 +1,12 @@
 package samux.cinematic;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -13,5 +15,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void registro(View view)
+    {
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
     }
 }
